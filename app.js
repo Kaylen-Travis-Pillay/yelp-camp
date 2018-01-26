688ef679ea3454cdf7fabf567ee28fe37b5ae142
Kaylen Travis Pillay <kaylen.student@gmail.com>
2018-01-26 13:02:51 +0200

    ejs package added, starting routes created

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+// Routes
 app.get("/", function(req, res){
-    res.send("Root directory reached");
+    res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds");
+});
+
+// Start server
 app.listen(3000, function(){
     console.log("yelp-camp server started");
 });
