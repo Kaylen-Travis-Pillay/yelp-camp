0a4b5322bf8db581bdb36aad3d188df620870a76
Kaylen Travis Pillay <kaylen.student@gmail.com>
2018-01-26 16:45:29 +0200

    YelpCamp: Creating Campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    { title: "Durban central park", image: "http://visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg" },
+    { title: "Johannesburg urban camp", image: "http://www.nationalparks.nsw.gov.au/~/media/45A30E448BED4691A5E6F0553EE023B5.ashx" },
+    { title: "Capetown seaside campground", image: "https://i.pinimg.com/originals/ab/5e/c4/ab5ec4ff722b1f65b855af71ba78c6fc.jpg" }
+];
 
 // Routes
 app.get("/", function(req, res){
@@ -9,12 +17,23 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        { title: "Durban central park", image: "http://visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg"},
-        { title: "Johannesburg urban camp", image: "http://www.nationalparks.nsw.gov.au/~/media/45A30E448BED4691A5E6F0553EE023B5.ashx"},
-        { title: "Capetown seaside campground", image: "https://i.pinimg.com/originals/ab/5e/c4/ab5ec4ff722b1f65b855af71ba78c6fc.jpg"}
-    ];
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var title = req.body.title;
+    var image = req.body.image;
+    var new_campground = {
+        title: title,
+        image: image
+    };
+
+    campgrounds.push(new_campground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new_campground");
 });
 
 // Start server
