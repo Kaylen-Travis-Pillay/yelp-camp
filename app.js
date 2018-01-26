813701b7bd6fb18a4f7af05a71295a5b19300237
Kaylen Travis Pillay <kaylen.student@gmail.com>
2018-01-26 13:20:22 +0200

    Campground examples added

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds");
+    var campgrounds = [
+        { title: "Durban central park", image: "http://visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg"},
+        { title: "Johannesburg urban camp", image: "http://www.nationalparks.nsw.gov.au/~/media/45A30E448BED4691A5E6F0553EE023B5.ashx"},
+        { title: "Capetown seaside campground", image: "https://i.pinimg.com/originals/ab/5e/c4/ab5ec4ff722b1f65b855af71ba78c6fc.jpg"}
+    ];
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 // Start server
