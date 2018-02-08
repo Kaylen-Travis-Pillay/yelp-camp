86e2abebe4cb902ee7acd3482b7ee2fe0a650633
Kaylen Travis Pillay <kaylen.student@gmail.com>
2018-02-08 20:59:27 +0200

    Added the comments structure

--- app.js
+++ app.js
@@ -73,6 +73,42 @@ app.get("/campgrounds/:id", function(req, res){
     });
 });
 
+// =====================
+// Comments ROUTE
+// =====================
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment._id);
+                    campground.save(function(err){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            res.redirect("/campgrounds/" + campground._id);
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
 // Start server on local machine
 app.listen(3000, function(){
     console.log("yelp-camp server started");
